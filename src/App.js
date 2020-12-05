7c163774f4318064fde8738f834b8b9dace05836
Abraham Hernandez <Abrahag40@gmail.com>
2020-12-05 01:15:56 -0600

    Carga de proyecto

--- src/App.js
+++ src/App.js
@@ -1,93 +1,79 @@
-import React, { useState } from 'react'
-import Header from './Components/Header'
-import Person from './Components/Person'
-import Lonely from './Components/Lonely'
-import data from './data.json'
-import './App.css'
+import React, { useState } from "react";
+import Header from "./Components/Header";
+import Person from "./Components/Person";
+import Lonely from "./Components/Lonely";
+import data from "./data.json";
+import "./App.css";
 
 function App() {
-  const [people, setPeople] = useState(data)
-  const [likedUsers, setLikedUsers] = useState(data)
-  const [superLikedUser, setsuperLikedUser] = useState(data)
-  const [disliked, setDisliked] = useState(data)
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = useState(data);
+  const [superLikedUser, setsuperLikedUser] = useState(data);
+  const [disliked, setDisliked] = useState(data);
   const activeUser = 0;
 
-  const removedPersonFromDataScr = (peopleSource, userId) => 
-    peopleSource.filter(user => user.id !== userId);
+  const removedPersonFromDataScr = (peopleSource, userId) =>
+    peopleSource.filter((user) => user.id !== userId);
 
-  const modifySuperficialChoices = ( userId, action ) => {
+  const modifySuperficialChoices = (userId, action) => {
     const newPeople = [...people];
-    const newLikedUsers = [...likedUsers]
-    const newSuperLikedUsers = [...superLikedUser]
-    const newDislikedUsers = [...disliked]
+    const newLikedUsers = [...likedUsers];
+    const newSuperLikedUsers = [...superLikedUser];
+    const newDislikedUsers = [...disliked];
 
     console.log(action);
 
-    switch(action){
-      case 'ADD_TO_LIKED_USERS':
-        
-        if(!people[activeUser].likedUsers.includes(userId)){
-
+    switch (action) {
+      case "ADD_TO_LIKED_USERS":
+        if (!people[activeUser].likedUsers.includes(userId)) {
           newPeople[activeUser].likedUsers.push(userId);
           newLikedUsers.push(data[userId]);
 
           setLikedUsers(newLikedUsers);
           setPeople(removedPersonFromDataScr(people, userId));
-
         }
         break;
-      case 'ADD_TO_DISLIKED_USERS':
-
-        if(!people[activeUser].dislikedUsers.includes(userId)){
-
+      case "ADD_TO_DISLIKED_USERS":
+        if (!people[activeUser].dislikedUsers.includes(userId)) {
           newPeople[activeUser].dislikedUsers.push(userId);
           newDislikedUsers.push(data[userId]);
 
           setDisliked(newLikedUsers);
           setPeople(removedPersonFromDataScr(people, userId));
-
         }
-
         break;
-      case 'ADD_TO_SUPERLIKED_USERS': 
-
-        if(!people[activeUser].superLikedUsers.includes(userId)){
-
+      case "ADD_TO_SUPERLIKED_USERS":
+        if (!people[activeUser].superLikedUsers.includes(userId)) {
           newPeople[activeUser].superLikedUsers.push(userId);
-          newSuperLikedUsers.push(data[userId])
+          newSuperLikedUsers.push(data[userId]);
 
-          newSuperLikedUsers(newSuperLikedUsers)
+          newSuperLikedUsers(newSuperLikedUsers);
           setPeople(removedPersonFromDataScr(people, userId));
-
         }
         break;
       default:
-        return people;
+      return people;
     }
-
-  }
+  };
 
   return (
     <div className="app">
       <Header />
 
-      {
-        people[1] ? (
-          <Person 
-            key={people[1].id}
-            person={people[1]}
-            modifySuperficialChoices={modifySuperficialChoices}
-            likedUsers={likedUsers}
-          />
-        ) : (
-
-          <Lonely 
-            activeUserImage={people[activeUser].image}
-            likedUsers={likedUsers}
-            superLikedUser={superLikedUser}
-          />
-        )
-      }
+      {people[1] ? (
+        <Person
+          key={people[1].id}
+          person={people[1]}
+          modifySuperficialChoices={modifySuperficialChoices}
+          likedUsers={likedUsers}
+        />
+      ) : (
+        <Lonely
+          activeUserImage={people[activeUser].image}
+          likedUsers={likedUsers}
+          superLikedUser={superLikedUser}
+        />
+      )}
     </div>
   );
 }
--- src/Components/Lonely.jsx
+++ src/Components/Lonely.jsx
@@ -3,20 +3,20 @@ import LikedPerson from './LikedPerson'
 
 const Loney = ({ activeUserImage, linkedUsers, superLinkedUsers }) => (
     <div className="lonely">
-        <p> There's no new around you </p>
+        <p> No hay nuevas personas cerca </p>
 
         <span className="pulse">
-            <img src={`/images/users/${activeUserImage}`} alt="You..."/>
+            <img src={`/images/users/${activeUserImage}`} alt="Tú..."/>
         </span>
 
         <div className="liked-people">
-            <p> {linkedUsers.length > 0 ? "People you liked... let's hope they like you too!" : ''} </p>
+            <p> {linkedUsers.length > 0 ? "Persionas que te gustaron... " : ''} </p>
 
             {linkedUsers.map(item => (
                 <LikedPerson key={item.id} person={item} />
             ))}
 
-            <p> {superLinkedUsers.length > 0 ? "People you super liked!" : ""} </p>
+            <p> {superLinkedUsers.length > 0 ? "Personas que te dieron superlke!" : ""} </p>
 
             { superLinkedUsers.map(item => (
                 <LikedPerson key={item.id} person={item} />
